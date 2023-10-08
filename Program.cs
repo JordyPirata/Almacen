296792106b964c18a953b36ad68c9613860772ef
JordyPirata <jordi77alva@gmail.com>
2023-10-07 23:45:07 -0600

    update

--- Program.cs
+++ Program.cs
@@ -1,14 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-using static System.Console;
-using Almacen.Client.Models;
-
-Teacher teacher = new Teacher
-{
-    Id = 1,
-    Name = "Juan",
-    LastName = "Perez"
-};
-
-
-
-WriteLine(teacher);
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
